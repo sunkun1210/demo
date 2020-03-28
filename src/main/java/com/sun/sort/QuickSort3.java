fdc6f04fb80c6742c1012587a316d33a4baba69c
sunkun <87983133@qq.com>
2020-03-28 23:16:27 +0800

    快速排序 马士兵

--- src/main/java/com/sun/sort/QuickSort3.java
+++ src/main/java/com/sun/sort/QuickSort3.java
@@ -3,41 +3,40 @@ package com.sun.sort;
 public class QuickSort3 {
     // 测试 QuickSort
     public static void main(String[] args) {
-        int[] arr = {7,3,2,8,1,9,5,4,6};
+        int[] arr ={4,6};
         sort(arr,0,arr.length-1);
-        for (Object i : arr) {
-            System.out.print(i+" ");
-        }
+        print(arr);
     }
 
-    private static void sort(int[] arr, int l, int r){
-        if (l>=r){
+    private static void sort(int[] arr, int leftBound, int rightBound){
+        if (leftBound>=rightBound){
             return;
         }
-        partition(arr, l, r);
+        int mid=partition(arr, leftBound, rightBound);
+        sort(arr,leftBound,mid-1);
+        sort(arr,mid+1,rightBound);
     }
 
-    private static void partition(int[] arr, int leftBound, int rightBound){
+    private static int  partition(int[] arr, int leftBound, int rightBound){
         //取右边界当轴
         int pivot=arr[rightBound];
         int left=leftBound;
         int right=rightBound-1;
-        while(left<right){
-            while (arr[left]<=pivot){
-                left++;
-            }
-            while (arr[right]>=pivot){
-                right--;
-            }
-            if (left<right){
-                swap(arr,left,right);
-            }
+        while(left<=right){
+            while (left<=right&&arr[left]<=pivot) left++;
+            while (left<=right&&arr[right]>pivot) right--;
+            if (left<right)  swap(arr,left,right);
         }
-        //把轴放到该放的正确位置上去
+        //把轴放到该放的正确位置上去 (轴一定比左边的小 不然left++)
         swap(arr,left,rightBound);
+        return left; //返回轴的位置
     }
 
-
+    static void print(int[] arr){
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
 
     private static void swap(int[] arr, int i, int j) {
         int t = arr[i];
