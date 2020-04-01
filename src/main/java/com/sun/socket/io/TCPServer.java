5b6c27be9d052abd66d7f2ac410f5c0e9a772da1
sunkun <87983133@qq.com>
2020-04-02 00:15:36 +0800

    NIO注释

--- src/main/java/com/sun/socket/io/TCPServer.java
+++ src/main/java/com/sun/socket/io/TCPServer.java
@@ -19,6 +19,11 @@ public class TCPServer {
             Socket s = ss.accept();
             System.out.println("a client connect!");
             DataInputStream dis = new DataInputStream(s.getInputStream());
+//            int read= s.getInputStream().read(new byte[]{1});
+//            if (read>0){
+//                System.out.println("==");
+//            }
+            //这样的话 read不阻塞 但是会丢失数据(当s1在发送数据的时候 已经是s2连接了) 而且accept堵塞
             /***
              * 一个客户端连上来 迟迟不发东西 程序就在这阻塞住了
              * 另一个客户端连的时候 必须经过accept方法，而accept方法必须在下次循环的时候才进的来
